e8394e365ac3d3b338cd2a21290c76ac935b2263
JD Cantrell <jdcantrell@gmail.com>
2011-12-04 21:53:24 -0800

    Clean up base script, stub in login logic

--- luggage.rb
+++ luggage.rb
@@ -1,100 +1,119 @@
+require 'camping'
+require 'camping/ar'
+require 'camping/session'
+
 Camping.goes :Luggage
 
-module Luggage::Models
-  class Item < Base
-    belongs_to :user 
-  end
+module Luggage
+  include Camping::Session
 
-  class User < Base 
-    has_many :items 
-  end
+  module Models
+    class Item < Base
+    end
 
-  class BasicFields < V 1.0
-    def self.up
-      create_table Item.table_name do |t|
-        t.string   :key
-        t.string   :name
-        t.string   :path
-        t.string   :filetype
-        t.timestamps
+    class BasicFields < V 1.0
+      def self.up
+        create_table :luggage_items do |t|
+          t.string   :key
+          t.string   :name
+          t.string   :path
+          t.string   :filetype
+          t.timestamps
+        end
       end
 
-      create_table User.table_name do |t|
-        t.int      :user_id
-        t.string   :name
+      def self.down
+       drop_table :luggage_items
+      end
+    end
+  end
+
+
+  module Controllers
+    class Index
+      def get
+        @time = Time.now
+        render :sundial
       end
     end
 
-    def self.down
-     drop_table Item.table_name
-     drop_table User.table_name
-    end
-  end
-
-
-end
-
-
-module Luggage::Controllers
-  class Index
-    def get
-      @time = Time.now
-      render :sundial
-    end
-  end
-
-  class Login
-    def get
-      render :login
-    end
-  end 
-
-  class Logout
-    def get
-      render :login
-    end
-  end
-
-  class Files
-    def get
-      render :files
-    end
-  end
-
-  class FilesX
-    def get(everything)
-      render :view_file
-    end
-  end
-end
-
-module Luggage::Views
-  def layout
-    html do
-      head do
-        title { "Luggage" }
+    class Login
+      def get
+        render :login
       end
-      body {self << yield }
+
+      def post
+        #TODO: make this do something useful
+        @password = Password.new(ENV['LUGGAGE_HASH'])
+        if ENV['LUGGAGE_USER'] == input.username and @password == input.password
+          @state.user_id = 1
+        end
+      end
+    end 
+
+    class Logout
+      def get
+        @state.user_id = nil
+        redirect Index
+      end
+    end
+
+    class Files
+      def get
+        require_login!
+        render :files
+      end
+    end
+
+    class FilesX
+      def get(everything)
+        render :view_file
+      end
     end
   end
 
-  def sundial
-    p "The current time is #{@time}"
+  module Views
+    def layout
+      html do
+        head do
+          title { "Luggage" }
+        end
+        body {self << yield }
+      end
+    end
+
+    def sundial
+      p "The current time is #{@time}"
+    end
+
+    def login
+      p "This is not yet ready!"
+    end
+
+    def logout
+      p "Poof you're logged out"
+    end
+    
+    def files
+      p "there will be files here"
+    end
+
+    def view_file
+      p "this is your specific file view"
+    end
   end
 
-  def login
-    p "This is not yet ready!"
+  module Helpers
+    def logged_in?
+      !!@state.user_id
+    end
+
+    def require_login!
+      unless logged_in?
+        redirect Controllers::Login
+        throw :halt
+      end
+    end
   end
 
-  def logout
-    p "Poof you're logged out"
-  end
-  
-  def files
-    p "there will be files here"
-  end
-
-  def view_file
-    p "this is your specific file view"
-  end
 end
