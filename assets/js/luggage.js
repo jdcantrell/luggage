f4c704f9fbf00831af5e6b993eca31432fbeff23
JD Cantrell <jdcantrell@gmail.com>
2011-12-21 08:07:14 -0800

    Display inline help when form does not validate

--- assets/js/luggage.js
+++ assets/js/luggage.js
@@ -4,4 +4,25 @@ $(function () {
   });
 
   $('.view-tabs').tabs()
+
+  //This code deals with the original upload form
+  $('#upload_input input').bind('change', function () {
+    if ($(this).val() !== "") {
+      $('#upload_input').removeClass('error')
+    }
+  })
+
+  $('#upload_form').bind('submit', function () {
+    if ($('#upload').val() == "") {
+      $('#upload_input').addClass('error')
+      event.preventDefault()
+    }
+    else {
+      $('#upload_input').removeClass('error')
+      $('#upload_submit').val('Uploading file...');
+      $('#upload_submit').button('loading');
+    }
+  });
+
+  //TODO: Add in File.API code (replaces the above forms)
 });
