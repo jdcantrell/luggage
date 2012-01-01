f212fddfaa5900815a7fa570b7805ebfb9bd26f9
JD Cantrell <jdcantrell@gmail.com>
2012-01-01 13:24:12 -0800

    Remove silly test code

--- hash.rb
+++ hash.rb
@@ -1,12 +1,6 @@
 require 'bcrypt'
 
-puts "Enter a string to hash:"
+puts "Enter a password to hash:"
 secret = gets.chomp()
 @pw = BCrypt::Password.create(secret)
 puts @pw
-
-hash = "$2a$10$.5TFnyGgY5fXaGF3hyhU8eN5vMcHO5MOt1YfULyRschHsZ5aeoaqy"
-@a = BCrypt::Password.new(hash)
-if (@a == secret)
-  puts "You pass"
-end
